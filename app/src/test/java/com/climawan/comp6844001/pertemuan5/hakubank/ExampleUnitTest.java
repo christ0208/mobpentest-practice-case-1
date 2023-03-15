78366a5fbedd37d001d0d49b3bc67963d914cd38
Christopher L <40509915+christ0208@users.noreply.github.com>
2023-03-15 16:44:07 +0700

    Initial state

---
+++ app/src/test/java/com/climawan/comp6844001/pertemuan5/hakubank/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.climawan.comp6844001.pertemuan5.hakubank;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
